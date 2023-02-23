744045bb540bc2b2f9a9d4a119eb7d3fba83b444
emanuele sacchetti <manusacco92@gmail.com>
2023-02-23 15:56:58 +0100

    ai click i console.log di prova funzionano ma alla creare value vuoti il programma non fa il suo lavoro e il pulsante cancella non cancella effettivamente

--- js/main.js
+++ js/main.js
@@ -11,17 +11,35 @@ const domButtonCancel = document.querySelector('#cancel');
 
 domButtonGenerate.addEventListener('click', 
     function () {
-        const domForNameText = domFormName.value;
-        console.log(domForNameText);
+        const domFormNameText = domFormName.value;
+        const domFormKmText = domFormKm.value;
+        const domFormAgeText = domFormAge.value;
+
+        //palesemente il value sulle option non mi legge il contenuto ma il suo value
+
+        console.log(domFormNameText + domFormAgeText + domFormKmText);
     }
 );
 
 
 domButtonCancel.addEventListener('click', 
     function() {
-        const domForNameText = domFormName.value;
-        console.log(domForNameText)
-        domForNameText.value = "";
+        const domFormNameText = domFormName.value;
+        const domFormKmText = domFormKm.value;
+        const domFormAgeText = domFormAge.value;
+
+        //date queste tre costanti al click, le costanti anvranno value vuoto
+        //MA NON SUCCEDE. in più il pulsante cancella ora non cencella più
+
+        domFormNameText.value = "";
+        domFormKmText.value = "";
+        domFormAgeText.value = "";
+
+        //anche se facendo la prova con il console.log il tutto sembra essere oprativo
+        //deduco ci sia un problema nell'ordinare al value di essere vuoto
+
+        console.log(domFormNameText + domFormAgeText + domFormKmText);
+
     }
 );
 
